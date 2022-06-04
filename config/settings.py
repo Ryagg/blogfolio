e391d7f1c8f8cccde8f099ded4074846de6c13b0
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-06-05 00:36:36 +0200

    feat: Add custom user model, forms + admin

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +136,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
