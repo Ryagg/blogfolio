9651c7c3301f372e89b34b27f8add71755d00e63
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-06-12 20:07:29 +0200

    feat: Add blog views

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('blog', views.PostList.as_view(), name='blog'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+]
