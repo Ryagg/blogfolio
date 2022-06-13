292e5e09dc683fc878f2f1af9d5fed1ca322700c
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-06-13 20:54:27 +0200

    feat: Log errors to file even when DEBUG=False

--- config/settings.py
+++ config/settings.py
@@ -192,3 +192,37 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 # ensure that INTERNAL_IPS matches that of the Docker host
 INTERNAL_IPS = [ip[:-1] + '1' for ip in ips]
+
+# log errors to a file even when DEBUG=False
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
+            'datefmt': '%d/%b/%Y %H:%M:%S',
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    },
+}
