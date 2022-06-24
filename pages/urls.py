8e6a42bb9bf32cb3b4064c1f700183c960b717eb
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-06-24 21:28:49 +0200

    feat: Add site notice + privacy policy
    Move templates into project level templates folder

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView
+from . import views
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('site_notice/', views.SiteNoticeView.as_view(), name='site_notice'),
+    path('privacy_policy/', views.PrivacyPolicyView.as_view(), name='privacy_policy'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,11 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class SiteNoticeView(TemplateView):
+    template_name = 'site_notice.html'
+
+
+class PrivacyPolicyView(TemplateView):
+    template_name = 'privacy_policy.html'
