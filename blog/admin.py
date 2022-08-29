b91c8c14062f46254b1d706115bab2884f46bfa7
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-08-29 14:47:51 +0200

    fix: Add linebreaks to displayed blog posts
    Capitalize name under blog posts
    Display blog post content in admin panel with format_html

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'content', 'created_on')
+    list_editable = ('content',)
     list_filter = ('status',)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+
+    def content_view(self, obj):
+        return format_html('{}', obj.content)
 
 
 admin.site.register(Post, PostAdmin)
