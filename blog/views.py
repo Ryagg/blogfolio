cb2e9ba10a4dced7c4a6cc2fbeca610976078d3b
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-08-29 23:24:17 +0200

    fix: Update queryset after post creation

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,9 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog.html'
 
+    def get_queryset(self):
+        return self.queryset.all()
+
 
 class PostDetail(generic.DetailView):
     model = Post
